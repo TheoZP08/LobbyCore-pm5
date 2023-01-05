1e21f6dc2edf41b9bd3f75d008d164d403f48f7f
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2023-01-05 10:57:09 +0300

    Update EventListener.php

--- src/NordMC/Event/EventListener.php
+++ src/NordMC/Event/EventListener.php
@@ -23,7 +23,7 @@ use Vecnavium\FormsUI\FormAPI;
 use Vecnavium\FormsUI\SimpleForm;
 use NordMC\LobbyCore;
 
-class EventListener extends PluginBase implements Listener {
+class EventListener implements Listener {
 
     private $plugin;
 
