27ce4994b417a7e6e4540c0408f61fa657c6c443
TheoZP08 <142341641+TheoZP08@users.noreply.github.com>
2023-08-19 11:47:55 +0100

    Update Protection.php

--- src/NordMC/Event/Protection.php
+++ src/NordMC/Event/Protection.php
@@ -2,61 +2,66 @@
 
 namespace NordMC\Event;
 
-use pocketmine\player\Player;
+use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat as MG;
 use pocketmine\utils\Config;
 use pocketmine\permission\DefaultPermissions;
 
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityExplosionPrimeEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+
 use NordMC\LobbyCore;
 
-class Protection implements \pocketmine\event\Listener {
+class Protection implements Listener {
 
-	public function onDamage(\pocketmine\event\entity\EntityDamageEvent $ev) {
-		$pl = $ev->getEntity();
-		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			    $ev->cancel();
-		}
-	}
-	
-	public function onExplosion(\pocketmine\event\entity\ExplosionPrimeEvent $ev) {
-		$ev->cancel();
-		
-	}
-	
-	public function onBreak(\pocketmine\event\block\BlockBreakEvent $ev) {
-		$pl = $ev->getPlayer();
-		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
+    public function onDamage(EntityDamageEvent $ev): void {
+        $pl = $ev->getEntity();
+        if ($pl instanceof Player && $pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
+            $ev->cancel();
+        }
+    }
+
+    public function onExplosion(EntityExplosionPrimeEvent $ev): void {
+        $ev->cancel();
+    }
+
+    public function onBreak(BlockBreakEvent $ev): void {
+        $pl = $ev->getPlayer();
+        if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
             if (!$pl->hasPermission("core.build") && !$pl->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-				$ev->cancel();
-                $pl->sendPopup("§cYou have don't permissions for break blocks");
-			} else {
+                $ev->cancel();
+                $pl->sendPopup("§cYou don't have permissions to break blocks");
             }
-		}
-	}
-	
-	public function onPlace(\pocketmine\event\block\BlockPlaceEvent $ev) {
-		$pl = $ev->getPlayer();
-		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-            if (!$pl->hasPermission("core.build") && !$pl->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-				$ev->cancel();
-                $pl->sendPopup("§cYou have don't permissions for place blocks");
-			} else {
-			}
-		}
-	}
-	
-	public function onDrop(\pocketmine\event\player\PlayerDropItemEvent $ev) {
-		$pl = $ev->getPlayer();
-		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			$ev->cancel();
-		}
-	}
+        }
+    }
 
-	public function onExhaust(\pocketmine\event\player\PlayerExhaustEvent $ev) {
-		$pl = $ev->getPlayer();
-		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			$ev->cancel();
-		}
-	}
+    public function onPlace(BlockPlaceEvent $ev): void {
+        $pl = $ev->getPlayer();
+        if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
+            if (!$pl->hasPermission("core.build") && !$pl->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                $ev->cancel();
+                $pl->sendPopup("§cYou don't have permissions to place blocks");
+            }
+        }
+    }
+
+    public function onDrop(PlayerDropItemEvent $ev): void {
+        $pl = $ev->getPlayer();
+        if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
+            $ev->cancel();
+        }
+    }
+
+    public function onExhaust(PlayerExhaustEvent $ev): void {
+        $pl = $ev->getPlayer();
+        if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
+            $ev->cancel();
+        }
+    }
 }
